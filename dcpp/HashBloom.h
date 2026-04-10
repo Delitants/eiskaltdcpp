e2444524e050adec7ea9bbedfbb7b222fb9acd2d
Neolo <admin@nlight.org.ua>
2026-04-10 13:58:26 -1000

    Port DC++ 0.883 bloom and allocator helpers

--- dcpp/HashBloom.h
+++ dcpp/HashBloom.h
@@ -1,21 +1,22 @@
 /*
-* Copyright (C) 2001-2019 Jacek Sieka, arnetheduck on gmail point com
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2001-2025 Jacek Sieka, arnetheduck on gmail point com
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-#pragma once
+#ifndef HASHBLOOM_H_
+#define HASHBLOOM_H_
 
 #include "typedefs.h"
 
@@ -32,26 +33,28 @@ namespace dcpp {
  */
 class HashBloom {
 public:
-    HashBloom() : k(0), h(0) { }
+	HashBloom() : k(0), h(0) { }
 
-    /** Return a suitable value for k based on n */
-    static size_t get_k(size_t n, size_t h);
-    /** Optimal number of bits to allocate for n elements when using k hashes */
-    static uint64_t get_m(size_t n, size_t k);
+	/** Return a suitable value for k based on n */
+	static size_t get_k(size_t n, size_t h);
+	/** Optimal number of bits to allocate for n elements when using k hashes */
+	static uint64_t get_m(size_t n, size_t k);
 
-    void add(const TTHValue& tth);
-    bool match(const TTHValue& tth) const;
-    void reset(size_t k, size_t m, size_t h);
-    void push_back(bool v);
+	void add(const TTHValue& tth);
+	bool match(const TTHValue& tth) const;
+	void reset(size_t k, size_t m, size_t h);
+	void push_back(bool v);
 
-    void copy_to(ByteVector& v) const;
+	void copy_to(ByteVector& v) const;
 private:
 
-    size_t pos(const TTHValue& tth, size_t n) const;
+	size_t pos(const TTHValue& tth, size_t n) const;
 
-    std::vector<bool> bloom;
-    size_t k;
-    size_t h;
+	std::vector<bool> bloom;
+	size_t k;
+	size_t h;
 };
 
 }
+
+#endif /*HASHBLOOM_H_*/
