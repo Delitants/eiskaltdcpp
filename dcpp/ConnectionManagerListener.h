01b8908d24b1e95296b07e2a4dbe21e865f29ae1
Neolo <admin@nlight.org.ua>
2026-04-10 14:56:22 -1000

    Port DC++ 0.883 connection manager listener with compatibility shim

--- dcpp/ConnectionManagerListener.h
+++ dcpp/ConnectionManagerListener.h
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
+ * Copyright (C) 2001-2025 Jacek Sieka, arnetheduck on gmail point com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#if !defined(CONNECTION_MANAGER_LISTENER_H)
+#define CONNECTION_MANAGER_LISTENER_H
 
 #include "forward.h"
 #include "typedefs.h"
@@ -24,20 +25,23 @@ namespace dcpp {
 
 class ConnectionManagerListener {
 public:
-    virtual ~ConnectionManagerListener() { }
-    template<int I> struct X { enum { TYPE = I }; };
+	virtual ~ConnectionManagerListener() { }
+	template<int I>	struct X { enum { TYPE = I }; };
 
-    typedef X<0> Added;
-    typedef X<1> Connected;
-    typedef X<2> Removed;
-    typedef X<3> Failed;
-    typedef X<4> StatusChanged;
+	typedef X<0> Added;
+	typedef X<1> Connected;
+	typedef X<2> Removed;
+	typedef X<3> Failed;
+	typedef X<4> StatusChanged;
 
-    virtual void on(Added, ConnectionQueueItem*) { }
-    virtual void on(Connected, ConnectionQueueItem*) { }
-    virtual void on(Removed, ConnectionQueueItem*) { }
-    virtual void on(Failed, ConnectionQueueItem*, const string&) { }
-    virtual void on(StatusChanged, ConnectionQueueItem*) { }
+	virtual void on(Added, ConnectionQueueItem*) noexcept { }
+	virtual void on(Connected, ConnectionQueueItem*) noexcept { }
+	virtual void on(Connected, ConnectionQueueItem*, UserConnection*) noexcept { }
+	virtual void on(Removed, ConnectionQueueItem*) noexcept { }
+	virtual void on(Failed, ConnectionQueueItem*, const string&) noexcept { }
+	virtual void on(StatusChanged, ConnectionQueueItem*) noexcept { }
 };
 
 } // namespace dcpp
+
+#endif // !defined(CONNECTION_MANAGER_LISTENER_H)
