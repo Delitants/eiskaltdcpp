3339fe699eed07ce4153e8ddfa84dc443333fbc9
Neolo <admin@nlight.org.ua>
2026-04-10 14:45:40 -1000

    Port DC++ 0.883 finished and log manager listener headers

--- dcpp/LogManagerListener.h
+++ dcpp/LogManagerListener.h
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2001-2019 Jacek Sieka, arnetheduck on gmail point com
+ * Copyright (C) 2001-2025 Jacek Sieka, arnetheduck on gmail point com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +15,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef DCPLUSPLUS_DCPP_LOG_MANAGER_LISTENER_H
+#define DCPLUSPLUS_DCPP_LOG_MANAGER_LISTENER_H
 
 #include <string>
 
@@ -25,11 +26,13 @@ using std::string;
 
 class LogManagerListener {
 public:
-    virtual ~LogManagerListener() { }
-    template<int I> struct X { enum { TYPE = I }; };
+	virtual ~LogManagerListener() { }
+	template<int I>	struct X { enum { TYPE = I }; };
 
-    typedef X<0> Message;
-    virtual void on(Message, time_t, const string&) { }
+	typedef X<0> Message;
+	virtual void on(Message, time_t, const string&) noexcept { }
 };
 
 } // namespace dcpp
+
+#endif // !defined(DCPLUSPLUS_DCPP_LOG_MANAGER_LISTENER_H)
