3b3a0ed7229b8d136e4d1f361aac1061108cffb5
Neolo <admin@nlight.org.ua>
2026-04-10 14:04:27 -1000

    Port DC++ 0.883 pointer helpers with local compatibility shims

--- dcpp/Pointer.h
+++ dcpp/Pointer.h
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2001-2012 Jacek Sieka, arnetheduck on gmail point com
- * Copyright (C) 2009-2019 EiskaltDC++ developers
+ * Copyright (C) 2001-2025 Jacek Sieka, arnetheduck on gmail point com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -16,13 +15,13 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#pragma once
+#ifndef DCPLUSPLUS_DCPP_POINTER_H
+#define DCPLUSPLUS_DCPP_POINTER_H
 
-#include "intrusive_ptr.h"
-
-#include <memory>
+#include <boost/smart_ptr/intrusive_ptr.hpp>
 
 #include <atomic>
+#include <memory>
 
 namespace dcpp {
 
@@ -33,32 +32,33 @@ template<typename T>
 class intrusive_ptr_base
 {
 public:
-    void inc() {
-        intrusive_ptr_add_ref(this);
-    }
+	void inc() {
+		intrusive_ptr_add_ref(this);
+	}
 
-    void dec() {
-        intrusive_ptr_release(this);
-    }
+	void dec() {
+		intrusive_ptr_release(this);
+	}
 
-    bool unique(int val = 1) const {
-        return (ref <= val);
-    }
+	bool unique(int val = 1) noexcept {
+		return (ref <= val);
+	}
 
 protected:
-    intrusive_ptr_base() : ref(0) { }
+	intrusive_ptr_base() noexcept : ref(0) { }
 
 private:
-    friend void intrusive_ptr_add_ref(intrusive_ptr_base* p) { ++p->ref; }
-    friend void intrusive_ptr_release(intrusive_ptr_base* p) { if(--p->ref == 0) { delete static_cast<T*>(p); } }
+	friend void intrusive_ptr_add_ref(intrusive_ptr_base* p) { ++p->ref; }
+	friend void intrusive_ptr_release(intrusive_ptr_base* p) { if(--p->ref == 0) { delete static_cast<T*>(p); } }
 
-    std::atomic<long> ref;
+	std::atomic_long ref;
 };
 
 struct DeleteFunction {
-    template<typename T>
-    void operator()(const T& p) const { delete p; }
+	template<typename T>
+	void operator()(const T& p) const { delete p; }
 };
 
 } // namespace dcpp
 
+#endif // !defined(POINTER_H)
